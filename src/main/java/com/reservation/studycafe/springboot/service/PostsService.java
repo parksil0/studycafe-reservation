9137291c2cdf792d7b3f151685035ce7fe1386d7
parksil <parksil00@naver.com>
2020-11-01 23:45:42 +0900

    게시판 전체조회, html페이지

--- src/main/java/com/reservation/studycafe/springboot/service/PostsService.java
+++ src/main/java/com/reservation/studycafe/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.reservation.studycafe.springboot.service;
 
 import com.reservation.studycafe.springboot.domain.posts.Posts;
 import com.reservation.studycafe.springboot.domain.posts.PostsRepository;
+import com.reservation.studycafe.springboot.web.dto.PostsListResponseDto;
 import com.reservation.studycafe.springboot.web.dto.PostsResponseDto;
 import com.reservation.studycafe.springboot.web.dto.PostsSaveRequestDto;
 import com.reservation.studycafe.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,11 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException(("해당 게시글이 없습니다. id=")+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }

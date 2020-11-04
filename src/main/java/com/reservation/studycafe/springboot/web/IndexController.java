3da7dc74596bf1ce9116cbde8fe9e531bbdd0e13
parksil <parksil00@naver.com>
2020-11-04 22:22:16 +0900

    로그인 버그 수정

--- src/main/java/com/reservation/studycafe/springboot/web/IndexController.java
+++ src/main/java/com/reservation/studycafe/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.reservation.studycafe.springboot.web;
 
+import com.reservation.studycafe.springboot.config.auth.LoginUser;
 import com.reservation.studycafe.springboot.config.auth.dto.SessionUser;
 import com.reservation.studycafe.springboot.service.PostsService;
 import com.reservation.studycafe.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser)httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
